29be8fc61532d5497dcd5eaa6b469eaac67a0fa4
Millan <jose_milla_9@hotmail.com>
2019-03-30 22:01:51 -0430

    para actualizar

--- components/ArteCard.vue
+++ components/ArteCard.vue
@@ -1,15 +1,24 @@
 <template>
   <v-layout>
-    <v-flex mt-5 xs12>
+    <v-flex xs12>
       <v-card class="bg">
         <img class="centroimg" src="~/assets/banners/bannerC.jpg">
         <v-container fill-height fluid>
           <v-layout column fill-height>
             <v-flex xs12 align-end flexbox>
+              <v-flex class="hidden-md-and-down">
+                <img class="venado" src="~/assets/banners/venad2.png">
+              </v-flex>
+              <v-flex fixed class="hidden-lg-and-up hidden-sm-and-down">
+                <img class="venado2" src="~/assets/banners/venad2.png">
+              </v-flex>
+              <v-flex fixed class="hidden-sm-and-up">
+                <img class="venado3" src="~/assets/banners/venad2.png">
+              </v-flex>
               <v-card-title class="margen">
-                <span class="titulo">Creando diseños conceptuales y personajes</span>
+                <span class="titulo">Arte y conceptos ultilizados para inspirar</span>
                 <div>
-                  <span class="intro">Arte digital.</span><br>
+                  <span class="intro">Busca lo que realmente disfrutas hacer</span><br>
 
                   <blockquote class="blockquote frase">
                     &#8220;Art is never finished, only abandoned.&#8221;
@@ -21,9 +30,6 @@
                   </blockquote>
                 </div>
               </v-card-title>
-              <v-flex justify-end align-end left>
-                <img class="venado" src="~/assets/banners/venad2.png">
-              </v-flex>
             </v-flex>
           </v-layout>
         </v-container>
@@ -35,6 +41,7 @@
 
 <style scoped>
 .intro {
+  color: black;
   font-size: 24px;
 }
 .frase {
@@ -44,7 +51,7 @@
 }
 .titulo {
   color: #ff0375;
-  font-size: 3em;
+  font-size: 4em;
 }
 .centroimg {
   width: 100%;
@@ -52,14 +59,26 @@
 }
 .venado {
   position: absolute;
-  margin-left: 1200px;
-  margin-top: -1000px;
+  margin-left: 1100px;
+  margin-top: -660px;
+}
+.venado2 {
+  position: absolute;
+  margin-top: -420px;
+  margin-left: 800px;
+  width: 400px;
+}
+.venado3 {
+  position: absolute;
+  margin-top: -140px;
+  margin-left: 220px;
+  width: 160px;
 }
 .bg {
-  background-color: white;
+  background: linear-gradient(yellow, white);
 }
 .margen {
   margin-left: 40px;
-  margin-top: 40px;
+  margin-top: 20px;
 }
 </style>
