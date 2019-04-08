badaab12c98862ddd2c1246f0fa88eb968b8e34e
Ronald Alonzo <alonzo.ronald@gmail.com>
2019-04-08 15:50:22 -0400

    prueba de formularios online

--- components/inicio/Bienvenida.vue
+++ components/inicio/Bienvenida.vue
@@ -12,39 +12,34 @@
           xs12
           sm8
           pa-1
-          ma-4
           fill-height
         >
           <v-card dark>
-            <v-card-title primary-title>
+            <v-card-title>
               <v-flex>
                 <p class="webicultura">
                   La webicultura es un concepto que desarrollamos y practicamos
                   todos los dias, y lo definimos como el proceso diario de resolver
                   problemas de forma creativa. En este concepto se mezclan el arte
                   con la ciencia, la ingenieria con la artesania, la realidad con la
-                  creatividad;
+                  creatividad
                 </p>
               </v-flex>
             </v-card-title>
             <v-card-text class="centro">
-              <v-container fluid>
-                <v-layout row wrap>
-                  <v-flex xs12 sm8 offset-sm2>
-                    <ul>
-                      <li class="mision">
-                        Identificamos los puntos de contacto entre tus productos y tus clientes
-                      </li>
-                      <li class="mision">
-                        Entendemos las oportunidades para influenciar a tus consumidores
-                      </li>
-                      <li class="mision">
-                        Ofrecemos a nuestros clientes la mejor experiencia en sus proyectos
-                      </li>
-                    </ul>
-                  </v-flex>
-                </v-layout>
-              </v-container>
+              <v-flex xs12 sm8 offset-sm2>
+                <ul>
+                  <li class="mision">
+                    Identificamos los puntos de contacto entre tus productos y tus clientes
+                  </li>
+                  <li class="mision">
+                    Entendemos las oportunidades para influenciar a tus consumidores
+                  </li>
+                  <li class="mision">
+                    Ofrecemos a nuestros clientes la mejor experiencia en sus proyectos
+                  </li>
+                </ul>
+              </v-flex>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -84,7 +79,6 @@ export default {}
   font-family: 'Helvetica' fantasy;
   font-weight: bolder;
   /* background-color: black; */
-  padding-top: 140px;
 }
 .mision {
   font-size: 1.4em;
